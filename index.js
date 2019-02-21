81c2da2f800edb32324130203a6ec01c177ccaa0
Bukola <bukola_jimoh@yahoo.co.uk>
2019-02-21 16:00:49 +0000

    change character properties paths

--- index.js
+++ index.js
@@ -8,67 +8,67 @@ const PORT = 4000;
 const app = express();
 
 app.use((req, res, next) => {
-    console.log(`This time is ${Date.now()}`);
-    next();
+  console.log(`This time is ${Date.now()}`);
+  next();
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello, welcome to SouthPark API!" });
+  res.json({ message: "Hello, welcome to SouthPark API!" });
 });
 
 app.get("/characterinfo", (req, res) => {
-    //const characters = req.query;
-    let all = library.getAllCharacters();
-    console.log(all);
-    res.json({ all });
+  //const characters = req.query;
+  let all = library.getAllCharacters();
+  console.log(all);
+  res.json({ all });
 });
 
 app.get("/name/:characterName", (req, res) => {
-    const characterName = req.params.characterName;
-    let character = library.getCharacterName(characterName);
-    console.log(character);
-    res.json({ character });
+  const characterName = req.params.characterName;
+  let character = library.getCharacterName(characterName);
+  console.log(character);
+  res.json({ character });
 });
 app.get("/gender/:characterName", (req, res) => {
-    const characterName = req.params.characterName;
-    let gender = library.getCharacterGender(characterName);
-    console.log(gender);
-    res.json({ gender });
+  const characterName = req.params.characterName;
+  let gender = library.getCharacterGender(characterName);
+  console.log(gender);
+  res.json({ gender });
 });
 
 app.get("/age/:characterName", (req, res) => {
-    const age = req.params.characterName;
-    let characterAge = library.getCharacterAge(age);
-    res.json({ characterAge });
+  const age = req.params.characterName;
+  let characterAge = library.getCharacterAge(age);
+  res.json({ characterAge });
 });
 
-app.get("/characternameinfo/:hairColor", (req, res) => {
-    const hairColor = req.params.hairColor;
-    let hair = library.getCharacterHair(hair);
-    res.json({ hair });
+app.get("/haircolor/:characterName", (req, res) => {
+  const name = req.params.characterName;
+  let hair = library.getCharacterHair(name);
+  res.json({ hair });
 });
 
-app.get("/characternameinfo/:occupation", (req, res) => {
-    const occupation = req.params.occupation;
-    let characterOccupation = library.getCharacterOccupation(occupation);
-    res.json({ characterOccupation });
+app.get("/occupation/:characterName", (req, res) => {
+  const name = req.params.characterName;
+  let characterOccupation = library.getCharacterOccupation(name);
+  res.json({ characterOccupation });
 });
 
-app.get("/characternameinfo/:grade", (req, res) => {
-    const grade = req.params.grade;
-    let characterGrade = library.getCharacterGrade(grade);
-    res.json({ getCharacterGrade });
+app.get("/grade/:characterName", (req, res) => {
+  const name = req.params.characterName;
+  let characterGrade = library.getCharacterGrade(name);
+  res.json({ characterGrade });
 });
 
-app.get("/characternameinfo/:image", (req, res) => {
-    const imageName = req.params.image;
-    let characterImage = library.getCharacterImage(imageName);
-    res.json({ characterImage });
+app.get("/image/:characterName", (req, res) => {
+  const name = req.params.characterName;
+  let characterImage = library.getCharacterImage(name);
+  res.json({ characterImage });
 });
 
 app.listen(PORT, () => {
-    console.log(`I'm listening on Port ${PORT}`);
+  console.log(`I'm listening on Port ${PORT}`);
 });
